044db738ce654a1107d07990cf368d9907ac32c7
AnnaGlinka <aglinka8@gmail.com>
2023-02-14 13:16:21 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
-import requests
 from requests import Session
-
+from pprint import pprint as pp
 import os
 
 API_KEY = os.getenv("API_KEY")
 
-#https://coinmarketcap.com/api/documentation/v1/
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
-
-#r = requests.get(url, headers=headers)
 
 
 class CMC:
+    #https://coinmarketcap.com/api/documentation/v1/
 
     def __init__(self, token):
         self.apiurl = 'https://pro-api.coinmarketcap.com'
-        self.headers = headers = {
+        self.headers = {
             'Accepts': 'application/json',
-            'X-CMC_PRO_API_KEY': API_KEY,
+            'X-CMC_PRO_API_KEY': token,
         }
         self.session = Session()
-        self.session.headers.update(headers=headers)
+        self.session.headers.update(self.headers)
+
+    def getAllCoins(self):
+        url = self.apiurl + '/v1/cryptocurrency/map'
+        r = self.session.get(url)
+        data = r.json()['data']
+        return data
+
+    def getPrice(self, symbol):
+        parameters = {'symbol': symbol}
+        url = self.apiurl + '/v1/cryptocurrency/quotes/latest'
+        r = self.session.get(url, params=parameters)
+        data = r.json()['data']
+        return data
+        
+        
 
 
 cmc = CMC(API_KEY)
+pp(cmc.getPrice('BTC'))
